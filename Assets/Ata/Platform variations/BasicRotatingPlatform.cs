6c06a4a627dcf1f8e6068b744cfaf30cf0c10e59
Ataeren aktun <aktun21@itu.edu.tr>
2025-05-30 11:15:16 +0300

    Platform Scriptleri eklendi

---
+++ Assets/Ata/Platform variations/BasicRotatingPlatform.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class BasicRotatingPlatform : MonoBehaviour
+{
+    [Header("Rotation Settings")]
+    [SerializeField] private float minRotationSpeed = 30f;
+    [SerializeField] private float maxRotationSpeed = 120f;
+    [SerializeField] private float speedChangeRate = 0.5f;
+
+    private float currentRotationSpeed;
+    private float targetRotationSpeed;
+
+    [Header("Platform Settings")]
+    [SerializeField] private bool canPlayerGrab = true;
+
+    void Start()
+    {
+        // Baþlangýç hýzýný rastgele belirle
+        currentRotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        targetRotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+    }
+
+    void Update()
+    {
+        // Hýz deðiþimi
+        currentRotationSpeed = Mathf.MoveTowards(currentRotationSpeed, targetRotationSpeed, speedChangeRate * Time.deltaTime);
+
+        // Hedef hýza ulaþtýysa yeni hedef belirle
+        if (Mathf.Approximately(currentRotationSpeed, targetRotationSpeed))
+        {
+            targetRotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        }
+
+        // Platformu döndür
+        transform.Rotate(0, 0, currentRotationSpeed * Time.deltaTime);
+    }
+
+    public bool CanPlayerGrab()
+    {
+        return canPlayerGrab;
+    }
+}
