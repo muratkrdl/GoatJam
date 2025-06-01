90926fc87807619d19db0f85e7e800385e8096df
Murat KIRDAL <muratkirdal0101@gmail.com>
2025-06-01 13:27:31 +0300

    MetreText

--- Assets/_Scripts/Controllers/MetresController.cs
+++ Assets/_Scripts/Controllers/MetresController.cs
@@ -14,9 +14,16 @@ namespace _Scripts.Controllers
         private int _currentMetres;
 
         private int _currentDistance;
+
+        private Vector2 _baseScale;
+
+        private Vector2 _maxReachPos;
         
         private void Start()
         {
+            _baseScale = metreText.transform.localScale;
+            _maxReachPos = playerHeadTransform.position;
+            
             _currentDistance = Mathf.Abs(Mathf.RoundToInt(playerHeadTransform.position.y - appleTransform.position.y));
             _maxMetres = _currentDistance * 5;
 
@@ -26,13 +33,19 @@ namespace _Scripts.Controllers
         private void Update()
         {
             CalculateMetres();
+            if (playerHeadTransform.position.y >= _maxReachPos.y)
+            {
+                _maxReachPos = playerHeadTransform.position;
+            }
         }
 
         private void CalculateMetres()
         {
-            _currentDistance = Mathf.Abs(Mathf.RoundToInt(playerHeadTransform.position.y - appleTransform.position.y));
+            _currentDistance = Mathf.Abs(Mathf.RoundToInt(_maxReachPos.y - appleTransform.position.y));
             _currentMetres = _maxMetres - _currentDistance * 5;
             metreText.text = _currentMetres.ToString();
+            
+            metreText.transform.localScale = _baseScale + new Vector2((float)_currentMetres/_maxMetres, (float)_currentMetres/_maxMetres);
         }
         
     }
